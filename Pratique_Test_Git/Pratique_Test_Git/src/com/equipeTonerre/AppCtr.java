40935a425ffb04a2832c4eca906d311e04999e5c
Karine Rock <72523330+karinerock@users.noreply.github.com>
2021-05-25 12:10:20 -0400

    Add files via upload

---
+++ Pratique_Test_Git/Pratique_Test_Git/src/com/equipeTonerre/AppCtr.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.equipeTonerre;
+
+/**
+ *
+ * @author karinerock
+ */
+public class AppCtr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Karine dit: Bonjour mes super co-équipières!");
+        System.out.println("Bon...mtn je vais faire un push");
+        System.out.println("Crime bine, je comprends rien...");
+    }
+    
+}
---
+++ Pratique_Test_Git/README.md
@@ -0,0 +1,5 @@
+# equipeDuTonerre
+Un Test pour l'équipe du Tonerre
+Veuillez rajouter un petit mot pour s'assurer qu'on est tous bien connectés!
+
+22 mai 2021-Voilà, j'ai ajouter un fichier NetBeans pour se pratiquer un peu... Alors, allez-y, faites une petite modification et faite un push!
